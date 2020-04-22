cc34193d5b2f3174e5fea0bd61e8b0e3046e3e46
Devoughn Hall <devoughnhall@Devoughns-MacBook-Pro.local>
2020-04-21 20:07:21 -0400

    Login Form complete

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+
+import Login from './components/Login'
+import PrivateRoute from './components/PrivateRoute'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li><Link to='/api/login'>Log In</Link></li>
+        </ul>
+        <Switch>
+          <Route path='/api/login' component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
